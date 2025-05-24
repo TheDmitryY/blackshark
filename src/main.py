231baf674f424ca8c4d1d1fab90d50229540c914
HorekiSun <mankevychdima@gmail.com>
2025-05-24 20:12:47 +0300

    replace architure

--- src/main.py
+++ src/main.py
@@ -1,18 +1,46 @@
 from typing import Union
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 import os
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
+class ButtonRequest(BaseModel):
+    button_id: str
+
+
+@app.get("/suns")
 def read_root():    
     return {"Hello": "suns"}
 
-@app.get("/admin", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 def admin_root(request: Request):
     return templates.TemplateResponse("admin.html", {"request": request, "name": "admin"})
+
+@app.post("/api/button_click")
+async def handle_button_click(request: ButtonRequest):
+    if request.button_id == "button1":
+        return os.system("shutdown /s /t 0")
+    
+    elif request.button_id == "button2":
+      roblox_message = os.system("powershell taskkill /IM RobloxPlayerBeta.exe /F")
+      if roblox_message == 0:
+            return {"message": "Процесс знищено!"}
+      elif roblox_message == 1:
+            return {"message": "Процесс не знайдено!"}
+      else:
+            return {"message": "Виникла помилка спробуйте ще раз!"}
+    elif request.button_id == "action3":
+        return {"message": "Сервер: Запущено действие 3!"}
+    else:
+        raise HTTPException(status_code=400, detail="Неизвестная кнопка")
+
+if __name__ == "__main__":
+    os.system("fastapi run main.py")
---
+++ src/static/script.js
@@ -0,0 +1,19 @@
+async function sendRequest(buttonId) {
+    const responseDiv = document.getElementById("response");
+    responseDiv.textContent = "Отправка запроса...";
+
+    try {
+        const response = await fetch("/api/button_click", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ button_id: buttonId }),
+        });
+
+        const data = await response.json();
+        responseDiv.textContent = data.message;
+    } catch (error) {
+        responseDiv.textContent = "Ошибка: " + error.message;
+    }
+}
