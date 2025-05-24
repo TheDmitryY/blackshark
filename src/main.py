816b77b54004aaa7c2d4e4173e8ced0d48b28424
HorekiSun <mankevychdima@gmail.com>
2025-05-24 16:29:05 +0300

    connect html to page

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def read_root():
+def read_root():    
     return {"Hello": "suns"}
 
-@app.get("/admin")
-def admin_root():
-    return "Welcome to administration panel!"
+@app.get("/admin", response_class=HTMLResponse)
+def admin_root(request: Request):
+    return templates.TemplateResponse("admin.html", {"request": request, "name": "admin"})
