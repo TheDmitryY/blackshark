2f98407ed84279ca7b4f8c489bc6f50792ba3b1d
HorekiSun <mankevychdima@gmail.com>
2025-05-24 17:43:37 +0300

    test

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from typing import Union
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
